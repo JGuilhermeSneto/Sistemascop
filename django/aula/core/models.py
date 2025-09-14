eeb55c7b627c6d6860fed43abc5fee99f3f8a754
José Guilherme da S. Neto <165957761+JGuilhermeSneto@users.noreply.github.com>
2025-09-14 13:24:11 -0300

    brincando com models e static

--- django/aula/core/models.py
+++ django/aula/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Perfil(models.Model):
+    nome = models.CharField(max_length = 200)
+    idade = models.IntegerField()
+    email = models.EmailField()
+    cpf = models.CharField(max_length = 11)
+
+    def __str__ (self):
+        return self.nome
